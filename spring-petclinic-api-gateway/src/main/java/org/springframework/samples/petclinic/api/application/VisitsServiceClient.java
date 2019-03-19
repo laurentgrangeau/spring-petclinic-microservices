3550dee777b53541201e8d13e9e80b06ec9e1a6f
Laurent Grangeau <laurent.grangeau@gmail.com>
2019-03-19 23:06:53 +0100

    Change code
    Signed-off-by: Laurent Grangeau <laurent.grangeau@gmail.com>

--- spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/application/VisitsServiceClient.java
+++ spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/application/VisitsServiceClient.java
@@ -41,7 +41,6 @@ public class VisitsServiceClient {
 
     private final RestTemplate loadBalancedRestTemplate;
 
-    @HystrixCommand(fallbackMethod = "emptyVisitsForPets")
     public Map<Integer, List<VisitDetails>> getVisitsForPets(final List<Integer> petIds) {
         UriComponentsBuilder builder = fromHttpUrl("http://visits-service/pets/visits")
             .queryParam("petId", joinIds(petIds));
@@ -54,9 +53,5 @@ public class VisitsServiceClient {
 
     private String joinIds(List<Integer> petIds) {
         return petIds.stream().map(Object::toString).collect(joining(","));
-    }
-
-    private Map<Integer, List<VisitDetails>> emptyVisitsForPets(List<Integer> petIds) {
-        return Collections.emptyMap();
     }
 }
